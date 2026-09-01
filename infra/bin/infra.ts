e7a70af99f43a89d50bacc892636cc42c63b51ba
Rafin31 <asifhossain976@gmail.com>
2026-09-01 11:10:25 +1000

    infra: group stack instantiations in app entrypoint

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -4,6 +4,6 @@ import { DataStack } from '../lib/data-stack';
 import { AuthStack } from '../lib/auth-stack';
 
 const app = new cdk.App();
-new DataStack(app, 'DataStack', {});
 
+new DataStack(app, 'DataStack', {});
 new AuthStack(app, 'AuthStack', {});
